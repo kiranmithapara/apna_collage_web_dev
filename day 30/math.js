1fe0cc9c5eea2672f425a8cc4f50180f567f08ba
Kiran Mithapara <kiranmithapara29@gmail.com>
2025-12-26 05:00:32 +0530

    add Day 33

---
+++ day 30/math.js
@@ -0,0 +1,18 @@
+// const sum = (a, b) => a + b;
+// const mul = (a, b) =>
+//   a * b;
+// ;
+
+// const pi = 3.14;
+
+// const obj = {
+//   sum: sum,
+//   mul: mul,
+//   pi: pi,
+// };
+
+// module.exports = obj;
+
+export const sum = (a, b) => a + b;
+export const mul = (a, b) => a * b;
+export const pi = 3.14;
---
+++ day 30/script.js
@@ -0,0 +1,21 @@
+// // const math = require("./math");
+// // // let name = "kiran";
+
+// // // console.log(name);
+
+// // // // process ee node js nu ek object che
+// // // //process.argv ee ek method che je apdne ek array return kare jema node file ni sathe je argument nakhvi te return kare
+// // // console.log(process.argv[3]); // first to argument system mathi avse
+
+// // console.log(math.sum(15, 5));
+// // console.log(math.pi);
+
+// const info = require("./Fruits");
+
+// console.log(info);
+// console.log(info[0].name);
+
+import { mul, pi } from "./math.js";
+
+console.log(mul(5, 2));
+console.log(pi);
